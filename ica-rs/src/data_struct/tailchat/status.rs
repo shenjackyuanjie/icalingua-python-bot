9002103c7bbfa962fec099df1eee99e2f1fbc85b
shenjack <3695888@qq.com>
2024-06-14 01:32:23 +0800

    试试新的共享方式，虽然略微麻烦，但至少比之前全局共享好

--- ica-rs/src/data_struct/tailchat/status.rs
+++ ica-rs/src/data_struct/tailchat/status.rs
@@ -1,4 +1,7 @@
+use std::sync::Arc;
+
 use serde::{Deserialize, Serialize};
+use tokio::sync::RwLock;
 
 use crate::data_struct::tailchat::UserId;
 
@@ -28,4 +31,32 @@ pub struct UpdateDMConverse {
     /// 更新时间
     #[serde(rename = "updatedAt")]
     pub updated_at: String,
+}
+
+pub type Writeable<T> = Arc<RwLock<T>>;
+
+#[derive(Debug, Clone)]
+pub struct BotStatus {
+    user_id: Writeable<UserId>,
+}
+
+impl BotStatus {
+    pub fn new(user_id: UserId) -> Self {
+        Self {
+            user_id: Arc::new(RwLock::new(user_id)),
+        }
+    }
+
+    pub async fn get_user_id(&self) -> UserId { self.user_id.read().await.clone() }
+
+    pub fn block_get_user_id(&self) -> UserId {
+        tokio::task::block_in_place(|| {
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            rt.block_on(self.get_user_id())
+        })
+    }
+
+    pub async fn set_user_id(&self, user_id: UserId) {
+        self.user_id.write().await.clone_from(&user_id);
+    }
 }
